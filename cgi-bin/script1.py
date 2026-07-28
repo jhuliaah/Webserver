ce3e84abbacfb000b1d93fe34a7bafc37b9293f9
Rafael Tanaka De Carvalho Freitas <ratanaka@c3r12p5.42sp.org.br>
2026-07-28 15:03:17 -0300

    pipes para o server do cgi arrumados

--- cgi-bin/script1.py
+++ cgi-bin/script1.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# 1. Preparamos o corpo da página
+body = "<html><body><h1>Fala, epoll! CGI em Alta Velocidade!</h1></body></html>\n"
+
+# 2. Imprimimos o cabeçalho HTTP completo (COM Content-Length!)
+print(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(body)}\r\n\r\n", end="")
+
+# 3. Imprimimos o corpo
+print(body, end="")
--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: ratanaka <ratanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 12:16:26 by ratanaka          #+#    #+#             */
-/*   Updated: 2026/06/23 15:29:56 by ratanaka         ###   ########.fr       */
+/*   Updated: 2026/07/20 17:54:30 by ratanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@
 #include <string>
 #include <cstddef>
 #include <ctime>
+#include <fstream>
 #include <sys/types.h>
+#include "WebServer.hpp"
+
 
 class Client {
 	public:
 		enum State {READING, WRITING, CGI_RUNNING, CLOSED};
+
 		struct CgiContext {
 			pid_t		pid;
 			int			stdin_fd;
@@ -28,14 +32,19 @@ class Client {
 			std::string outputBuffer;
 			size_t		inputSent;
 			time_t		startTime;
-		} _cgiContext; // copiei CgiContext de vados-sa
+
+			CgiContext() : pid(-1), stdin_fd(-1), stdout_fd(-1), inputSent(false), startTime(0) {}
+		};
 
 	private:
-		int			_fd;
-		time_t		_lastActivity;
-		std::string	_rawRequest;
-		std::string	_response;
-		State		_state_e;
+		int				_fd;
+		time_t			_lastActivity;
+		std::string		_rawRequest;
+		std::string		_response;
+		State			_state_e;
+
+		HttpRequest		_request;
+		CgiContext		_cgiContext;
 		//lembranças do norminette: typedef = _t, struct = _s, enum = _e
 
 		std::string _buildStaticResponse();
@@ -50,8 +59,13 @@ class Client {
 
 		int		getFd() const { return _fd; }
 		State	getState() const { return _state_e; }
+		void	setState(State state) {_state_e = state;}
 
+		CgiContext& getCgiContext() {return _cgiContext;}
+		HttpRequest& getRequest() {return _request;}
+		std::string&	getRawRequest() {return _rawRequest;}
 
+		void	setResponse(const std::string& res) { _response = res;}
 };
 
 #endif
