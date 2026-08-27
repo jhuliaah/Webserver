bd94c39dc8d19d46a9e5d5638b905d77046cad58
jhulia <jhuliaah@hotmail.com>
2026-08-27 15:55:41 -0300

    só falta testar

--- srcs/HttpParser.cpp
+++ srcs/HttpParser.cpp
@@ -5,23 +5,15 @@ HttpParser::HttpParser() : _state(REQUEST_LINE) {}
 
 HttpParser::~HttpParser() {}
 
-static std::string trim(const std::string& s)
-{
-    size_t start = s.find_first_not_of("\t");
-    if (start == std::string::npos)
-        return ("");
-    size_t end = s.find_last_not_of( "\t\r");
-    return s.substr(start, end - start + 1);
-}
-
 bool HttpParser::parseRequestLineFrom(const std::string& raw, size_t& pos, HttpRequest& req)
 {
+    (void)req;
     size_t lineEnd = raw.find("\r\n", pos);
-    if (lineEnd == std::string::npos);
+    if (lineEnd == std::string::npos)
         return (false);
-    
+
     std::string line = raw.substr(pos, lineEnd - pos);
     pos = lineEnd + 2;
 
-    
+    return (false);
 }
